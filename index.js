fcbfd5f03bea33c7f219afe05da3cfc08c0cf224
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-02-19 14:24:21 -0600

    try this

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-directions(abrivations = N, S, E, W);
-return North;
+ let abriviations = {
+   "N": "North";
+    "S":"South";
+    "E": "East";
+    "W": "West";
+ };
+document.getElementById("results").innerHTML = abriviations.N;
+
+
 
